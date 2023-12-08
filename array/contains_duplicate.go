6269f419a70d521b89968fcd26ca6b45686777a9
M. Rafiul Alam <rafiul2111@gmail.com>
2023-12-09 01:10:49 +0600

    add solution to contains duplicate

---
+++ array/contains_duplicate.go
@@ -0,0 +1,32 @@
+package array
+
+//Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+//
+//
+//
+//Example 1:
+//
+//Input: nums = [1,2,3,1]
+//Output: true
+//Example 2:
+//
+//Input: nums = [1,2,3,4]
+//Output: false
+//Example 3:
+//
+//Input: nums = [1,1,1,3,3,4,3,2,4,2]
+//Output: true
+
+func ContainsDuplicate(nums []int) bool {
+	seen := make(map[int]bool)
+
+	for i := range nums {
+		n := nums[i]
+		if seen[n] {
+			return true
+		}
+		seen[nums[i]] = true
+	}
+
+	return false
+}
