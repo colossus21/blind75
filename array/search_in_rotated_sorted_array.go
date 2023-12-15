439ad6b798689f566b0ae3250818f1323069a844
M. Rafiul Alam <rafiul2111@gmail.com>
2023-12-15 21:06:48 +0600

    add log to search in rotated array problem

--- array/search_in_rotated_sorted_array.go
+++ array/search_in_rotated_sorted_array.go
@@ -1,5 +1,7 @@
 package array
 
+import "fmt"
+
 /*
 	left := 0
 	right := len(nums) - 1
@@ -20,31 +22,67 @@ package array
 func Search(nums []int, target int) int {
 	// Find the lowest number
 	left := 0
-	right := len(nums)
+	right := len(nums) - 1
 
 	/*
-		1 2 3 4 5
-		1..3..5 M < R
-		1 2 3 4 5
-		1..3..5 M > R
+		L < M < R: 1 2 3
+		L < M > R: 1 3 2
+		L > M < R: 3 1 2
+		L > M > R: 3 2 1 (NOT POSSIBLE)
 	*/
 
 	index := -1
-
+	fmt.Println(nums)
 	for left < right {
 		mid := (left + right) / 2
 
 		if nums[mid] == target {
 			index = mid
+			break
 		}
+		fmt.Println("A L=", left, "R=", right, "M=", mid, " [ L , R, M ] =", "[", nums[left], ",", nums[mid], ",", nums[right], "]")
+		/*
+			1 2 3 4 5	-L < R
+			5 1 2 3 4	=L > R	-L > M
+			4 5 1 2 3	=L > R	-L > M
+			3 4 5 1 2	=L > R	=L < M
+			2 3 4 5 1	=L > R	=L < M
+		*/
 
-		// Lower side on left
-		if nums[mid] > nums[right] {
-			left = mid + 1
+		perfectlySorted := nums[left] <= nums[right]
+		if perfectlySorted {
+			fmt.Println("SORTED LOGIC")
+			if target <= nums[mid] {
+				left = mid + 1
+			} else {
+				right = mid
+			}
 		} else {
-			right = mid
+			if nums[left] > nums[mid] {
+				if target <= nums[mid] {
+					fmt.Println("L > M && T <= M :: R = M")
+					right = mid
+				} else {
+					fmt.Println("L > M && T > M :: L = M")
+					left = mid + 1
+				}
+			} else {
+				if target <= nums[mid] {
+					fmt.Println("L <= M && T <= M :: R = M")
+					right = mid
+				} else {
+					fmt.Println("L <= M && T <= M :: L = M")
+					left = mid + 1
+				}
+			}
 		}
+		fmt.Println("B L=", left, "R=", right, "M=", mid)
 	}
-
+	if nums[left] == target {
+		return left
+	}
+	if nums[right] == target {
+		return right
+	}
 	return index
 }
